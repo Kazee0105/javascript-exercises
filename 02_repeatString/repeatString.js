86e0d7e8bfbac35084af2d36bc7da0d3b9e0cd8e
Kazee PC <masuda144@hotmail.com>
2023-06-29 21:49:26 +0900

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeat = ""
+    if(num>0){
+        for(i=0;i<num;i++){
+            repeat += str;
+        }
+    }
+    else if(num === 0){
+        return ""
+    }
+    else{
+        return 'ERROR'
+    }
+    return repeat;
 };
+
+console.log(repeatString('hey',0));
 
 // Do not edit below this line
 module.exports = repeatString;
