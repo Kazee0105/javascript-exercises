3acc8c254830e418d58ce776a42a348edc76e65e
Kazee <masuda144@hotmail.com>
2023-06-30 22:41:03 +0900

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    if(typeof a !== typeof b) return "ERROR";
+    if(a < 0 || b < 0) return "ERROR";
 
-};
+    if(a > b){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    for(i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
+}
 
+console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
