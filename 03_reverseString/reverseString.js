18da6d94b9213a048eb15b3a96991e793f82e10e
Kazee <masuda144@hotmail.com>
2023-06-30 09:09:47 +0900

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverse = "";
+    for (i = 0; i <= str.length; i++){
+        reverse += str.charAt(str.length -i);
+    }
+    return reverse;
 };
+
+console.log(reverseString('Hello'))
 
 // Do not edit below this line
 module.exports = reverseString;
