20a2505ad363e5a46eafbd41833bc4b518b8b399
Kazee <masuda144@hotmail.com>
2023-06-30 15:13:35 +0900

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr){
+    let length = arguments.length
+  
+  for(i = 1; i < arguments.length; i++){
+      index = arr.indexOf(arguments[i]);
+    if(index > -1){
+        arr.splice(index,1)
+    }
+  }
+    return arr;
 
-};
-
+}
 // Do not edit below this line
 module.exports = removeFromArray;
