ab889870c28ebd28ed5a1d9c2b3bb535451f493a
Kazee PC <masuda144@hotmail.com>
2023-07-01 09:18:35 +0900

    07 updated

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let convert = (far - 32) * (5/9) 
+  let toNum = convert % 1 ? parseFloat(convert).toFixed(1) : convert;
+  console.log(typeof toNum)
+return toNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let convert = cel * (9/5) + 32;
+  console.log(typeof convert)
+  return convert % 1 ? parseFloat(convert).toFixed(1) : convert;
 };
 
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(73.2));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
