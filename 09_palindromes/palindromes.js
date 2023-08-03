0c47aae660b6bb53eaf38afb6e9020543befb1d3
Kazee PC <masuda144@hotmail.com>
2023-08-03 23:25:11 +0900

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+//filter only letters with regex.
+let filterLet = string.toLowerCase().split('').filter(char => /[a-zA-Z0-9]/.test(char));
+console.log(filterLet)
 
+return filterLet.join('') === filterLet.reverse().join('');
 };
 
 // Do not edit below this line
