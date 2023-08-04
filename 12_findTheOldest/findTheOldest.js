46c864fa2cc205418d37c0a339016eb66e8b5722
Kazee PC <masuda144@hotmail.com>
2023-08-05 00:42:42 +0900

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let thisYear = new Date().getFullYear();
+    for (i = 0; i < array.length; i++){
+        if(array[i].yearOfDeath === undefined){
+            array[i].yearOfDeath = thisYear;
+        } 
+    }
+    let ages = array.map((age) => age.yearOfDeath - age.yearOfBirth);
+    let oldest = ages.indexOf(Math.max(...ages));
 
+    return array[oldest];
 };
 
 // Do not edit below this line
