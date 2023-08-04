3e375f8b390d992a70c86db5359273a910727fd7
Kazee PC <masuda144@hotmail.com>
2023-08-04 22:53:56 +0900

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+ const fibonacci = function(num) {
+    let p = 0, c = 1, arr = [];
 
+    if(num < 0) return "OOPS"
+    for (i = 0; i < num; i++){
+        if(arr.length < 2){
+            arr.push(c);
+        }else {
+            p = arr[i - 2];
+            c = arr[i - 1];
+            arr.push(p + c);
+        }
+    }
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
